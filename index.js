588bd5c1b6f12768c89c637e2585b8ef998a5a99
Your Name <your.email@example.com>
2024-07-03 12:14:36 +0100

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Select all elements with the class 'clickable-link'
+//   const clickableLinks = document.querySelectorAll(".click");
+
+//   // Iterate over each element and add a click event listener
+//   clickableLinks.forEach(function (link) {
+//     link.addEventListener("click", function (event) {
+//       // Prevent the default action of the link
+//       event.preventDefault();
+
+//       // Add the 'clicked' class to the clicked link
+//       link.classList.add("link");
+//     });
+//   });
+// });
